2ed81a452660579db5cd854a16ea6c6f13eb960f
KaveyZheng <kaveyzheng1@gmail.com>
2023-03-04 13:10:07 -0500

    Initial commit

---
+++ Flixster/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  Flixster
+//
+//  Created by Kavey Zheng on 3/3/23.
+//
+
+import Foundation
---
+++ Flixster/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Flixster
+//
+//  Created by Kavey Zheng on 2/28/23.
+//
+
+import Foundation
